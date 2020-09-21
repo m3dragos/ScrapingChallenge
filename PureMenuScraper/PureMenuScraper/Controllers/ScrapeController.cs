537ce2fa3c148768ed8decdc4d8341361ce31670
Dragos Bursacovschi <dragosbursacovschi@smedix.com>
2020-09-21 12:35:05 +0300

    Initial project creation

---
+++ PureMenuScraper/PureMenuScraper/Controllers/ScrapeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PureMenuScraper.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScrapeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "It works!";
+        }
+    }
+}
